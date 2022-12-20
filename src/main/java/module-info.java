ee5221af067a81539dbd1f10c2d7e4bc73973c0a
mcoder01 <manu01141@gmail.com>
2022-12-20 15:38:32 +0100

    Project initial upload

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module unipa.prog3.prog3 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens unipa.prog3.prog3 to javafx.fxml;
+    exports unipa.prog3.prog3;
+}
